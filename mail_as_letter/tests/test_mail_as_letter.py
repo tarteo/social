3ba5b668d217af1cd9d46a731a9d6d0e10f358ed
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2016-08-19 10:10:41 +0200

    [ADD] mail_as_letter

---
+++ mail_as_letter/tests/test_mail_as_letter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestMailAsLetter(TransactionCase):
+
+    def setUp(self):
+        super(TestMailAsLetter, self).setUp()
+
+    def test_1(self):
+        pass
